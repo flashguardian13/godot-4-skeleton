84c9b574971af7b48c6da6a9f8d2ffa3fcef6e2f
Ben Amos <flashguardian13@gmail.com>
2023-08-03 12:36:47 -0700

    improved AI

--- scenes/screens/tic_tac_toe.gd
+++ scenes/screens/tic_tac_toe.gd
@@ -28,7 +28,7 @@ func _ready():
 			mark.board_marked.connect(_on_board_marked.bind(x, y, "O"))
 			mark.board_x = x
 			mark.board_y = y
-	
+
 	reset()
 
 func reset() -> void:
@@ -68,16 +68,16 @@ func _on_board_marked(x:int, y:int, symbol:String) -> void:
 	board_state[y][x] = symbol
 	_update_marks_from_state()
 	_disable_all_marks()
-		
+
 	if _get_winner() != " ":
 		$UI/ResetButton.visible = true
 		return
-	
+
 	if symbol == "O":
 		call_deferred("_do_computer_turn")
 	else:
 		_enable_all_unmarked()
-	
+
 func _get_winner() -> String:
 	print("[TicTacToe] _get_winner()")
 	for y in board_state.size():
@@ -86,29 +86,29 @@ func _get_winner() -> String:
 			return "O"
 		if _entire_array_equals(row, "X"):
 			return "X"
-	
+
 	for x in board_state.size():
 		var column:Array = _get_column(x)
 		if _entire_array_equals(column, "O"):
 			return "O"
 		if _entire_array_equals(column, "X"):
 			return "X"
-	
+
 	var diagonal:Array = _get_diagonal()
 	if _entire_array_equals(diagonal, "O"):
 		return "O"
 	if _entire_array_equals(diagonal, "X"):
 		return "X"
-	
-	diagonal = get_reverse_diagonal()
+
+	diagonal = _get_reverse_diagonal()
 	if _entire_array_equals(diagonal, "O"):
 		return "O"
 	if _entire_array_equals(diagonal, "X"):
 		return "X"
-	
+
 	if _is_board_full():
 		return "?"
-	
+
 	return " "
 
 func _get_row(y:int) -> Array:
@@ -123,7 +123,7 @@ func _get_diagonal() -> Array:
 		diagonal.push_back(board_state[y][y])
 	return diagonal
 
-func get_reverse_diagonal() -> Array:
+func _get_reverse_diagonal() -> Array:
 	var diagonal:Array = []
 	for y in board_state.size():
 		diagonal.push_back(board_state[y][board_state.size() - y - 1])
@@ -146,6 +146,79 @@ func _do_computer_turn() -> void:
 		for x in board_state[y].size():
 			if board_state[y][x] == " ":
 				empty_spaces.push_back(Vector2(x, y))
-	
-	var pos:Vector2 = empty_spaces.pick_random()
+
+	var scores:Array = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+	var score:int = 0
+	var o_count = 0
+	var x_count = 0
+
+	for y in board_state.size():
+		var row:Array = _get_row(y)
+		o_count = row.count("O")
+		x_count = row.count("X")
+		if o_count > 0 && x_count < 1:
+			score = o_count * o_count * 2
+		elif x_count > 0 && o_count < 1:
+			score = x_count * x_count
+		elif row.count(" ") == row.size():
+			score = 1
+		else:
+			score = 0
+		for x in row.size():
+			scores[y][x] += score
+
+	for x in board_state.size():
+		var column:Array = _get_column(x)
+		o_count = column.count("O")
+		x_count = column.count("X")
+		if o_count > 0 && x_count < 1:
+			score = o_count * o_count * 2
+		elif x_count > 0 && o_count < 1:
+			score = x_count * x_count
+		elif column.count(" ") == column.size():
+			score = 1
+		else:
+			score = 0
+		for y in column.size():
+			scores[y][x] += score
+
+	var diagonal:Array = _get_diagonal()
+	o_count = diagonal.count("O")
+	x_count = diagonal.count("X")
+	if o_count > 0 && x_count < 1:
+		score = o_count * o_count * 2
+	elif x_count > 0 && o_count < 1:
+		score = x_count * x_count
+	elif diagonal.count(" ") == diagonal.size():
+		score = 1
+	else:
+		score = 0
+	for y in diagonal.size():
+		scores[y][y] += score
+
+	diagonal = _get_reverse_diagonal()
+	o_count = diagonal.count("O")
+	x_count = diagonal.count("X")
+	if o_count > 0 && x_count < 1:
+		score = o_count * o_count * 2
+	elif x_count > 0 && o_count < 1:
+		score = x_count * x_count
+	elif diagonal.count(" ") == diagonal.size():
+		score = 1
+	else:
+		score = 0
+	for y in diagonal.size():
+		scores[y][diagonal.size() - y - 1] += score
+
+	print("  scores: %s" % ", ".join(scores))
+
+	var spaces_by_score:Dictionary = {}
+	for space in empty_spaces:
+		score = scores[space.y][space.x]
+		if !spaces_by_score.has(score):
+			spaces_by_score[score] = []
+		spaces_by_score[score].push_back(space)
+	var best_score:int = spaces_by_score.keys().max()
+
+	var pos:Vector2 = spaces_by_score[best_score].pick_random()
 	_on_board_marked(pos.x, pos.y, "X")
